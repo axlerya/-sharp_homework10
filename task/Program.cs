e3a589ee3c8688ab15ab8dee4a561075aa6e5e6b
axlerya <white_cat_02@mail.ru>
2023-09-21 18:13:14 +0500

    feature: add task
    add a solution to the task for the control work

---
+++ task/Program.cs
@@ -0,0 +1,21 @@
+﻿string[] arrayBefore = { "Hello", "2", "world", ":-)" };
+//string[] arrayBefore = { "Russia", "Denmark", "Kazan" };
+
+string[] NewArray(string[] arrBefore)
+{
+    string[] arrAfter = new string[arrBefore.Length];
+    int count = 0;
+
+    for (int i = 0; i < arrBefore.Length; i++)
+    {
+        if (arrBefore[i].Length < 4)
+        {
+            arrAfter[count] = arrBefore[i];
+            count++;
+        }
+    }
+    Array.Resize(ref arrAfter, count);
+    return arrAfter;
+}
+
+Console.WriteLine("[" + String.Join(", ", NewArray(arrayBefore)) + "]");
